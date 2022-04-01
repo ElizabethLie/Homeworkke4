f229e287c40407602110212949cdbfb74bc24c4b
elizabeth christina <19102094@ittelkom-pwt.ac.id>
2022-04-01 13:59:30 +0700

    add select and deselect songs

--- src/components/Song/index.js
+++ src/components/Song/index.js
@@ -1,13 +1,14 @@
-const playlistdetail = ({ image, title, album, url, artists }) => {
-    return (
-      <div className="App">
-          <img className="img-1" src={image}/>
-          <h3>{title}</h3>
-          <h4>{album}</h4>
-          <p>{artists}</p>
-          <button className="btn-1"><a href={url}>Select</a></button>
-      </div>
-    );
-  };
-  
-  export default playlistdetail;
+import Button from "./button";
+const playlistdetail = ({ image, title, album, artists }) => {
+  return (
+    <div className="App">
+        <img className="img-1" src={image}/>
+        <h3>{title}</h3>
+        <h4>{album}</h4>
+        <p>{artists}</p>
+        <Button/>
+    </div>
+  );
+};
+
+export default playlistdetail;
